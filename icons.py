1b5b08c1a69e0d7bb71442f67d0d648222f7e642
jrbgit <joanas@gmail.com>
2022-11-24 21:22:45 -0500

    base files for code

---
+++ icons.py
@@ -0,0 +1,20 @@
+import json
+import subprocess
+import os.path
+import time
+from decouple import config
+
+LCW_API_KEY = config('LCW_API_KEY')
+
+print()
+
+# Start Loop
+
+# Grab some LCW API results
+
+# Download the icons
+
+# Done
+
+print("Done")
+print()
